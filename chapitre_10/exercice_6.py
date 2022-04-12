df2880a0d3f23cda6e553ee1a455a3f09a064639
roman2veces <romanroman2705@gmail.com>
2022-04-11 20:24:32 -0400

    finish exercice 6 chapter 10

--- chapitre_10/exercice_6.py
+++ chapitre_10/exercice_6.py
@@ -1,10 +1,18 @@
-from sympy import *
+from scipy.integrate import quad 
+import matplotlib.pyplot as plt
+import numpy as np
 
-x, y = symbols("x, y")
+# Calculate integrale e^(-x^2) between -infinity and infinity 
+def f(x):
+    return np.exp((-1*x) ** 2)
 
-# print(integrate(exp((-1*x)**2), (x, -oo, oo)))
+integrale, err = quad(f, 1*np.inf, np.inf)
+print(f"Integrale: {integrale} Error: {err}")
 
-# print(integrate( 1/x), ()) )
+# Display integrale e^(-x^2) between -4 and 4
+x = np.arange(-4, 4, 0.001)
 
-
-print(integrate(sin(x)))
+plt.figure(figsize=(12,6))
+plt.plot(x, f(x))
+plt.title('e^(-x^2)')
+plt.show()
